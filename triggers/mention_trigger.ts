fc34614ed3093debf9470d81781aec8a32f8ba52
SpawnageLoong <richardloongcj@gmail.com>
2024-04-30 14:20:12 +0700

    Fix event rigger

--- triggers/mention_trigger.ts
+++ triggers/mention_trigger.ts
@@ -1,17 +1,26 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
 import sayHiWorkflow from "../workflows/say_hi_workflow.ts";
-import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
+import {
+  TriggerContextData,
+  TriggerEventTypes,
+  TriggerTypes,
+} from "deno-slack-api/mod.ts";
 
 const mentionTrigger: Trigger<typeof sayHiWorkflow.definition> = {
-  type: TriggerTypes.Shortcut,
+  type: TriggerTypes.Event,
   name: "Say Hi!",
+  description: "responds to a mention",
   workflow: `#/workflows/${sayHiWorkflow.definition.callback_id}`,
+  event: {
+    event_type: TriggerEventTypes.AppMentioned,
+    all_resources: true,
+  },
   inputs: {
     user_id: {
-      value: TriggerContextData.Shortcut.user_id,
+      value: TriggerContextData.Event.AppMentioned.user_id,
     },
     channel_id: {
-      value: TriggerContextData.Shortcut.channel_id,
+      value: TriggerContextData.Event.AppMentioned.channel_id,
     },
   },
 };
